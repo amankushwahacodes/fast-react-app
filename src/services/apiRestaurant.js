1779360ef8e4058fab97245388084686b29e0b1f
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-10-10 21:04:07 +0530

    Added error handling

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -26,7 +26,7 @@ export async function createOrder(newOrder) {
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+     });
 
     if (!res.ok) throw Error();
     const { data } = await res.json();
